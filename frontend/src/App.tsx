dbfcec1a4b4bbbafdc00fb75c1b259c33bc8b9b4
mayaj971307 <jullyannabas@gmail.com>
2023-05-17 09:58:00 +0800

    added bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,40 @@
-import { useState } from 'react';
-import './App.css';
+import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
 import { sampleProducts } from './data';
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <div>
-      <header>Aquilas Store</header>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg="dark" variant="dark" expand='lg' >
+          <Container>
+            <Navbar.Brand>Aquilas Store </Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">Cart</a>
+            <a href="/signin" className="nav-link">Sign In</a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-        {
-          sampleProducts.map((product) => (
-          <li key={product.slug}>
+        <Container className='mt-3'> 
+          <Row>
+        {sampleProducts.map((product) => (
+          <Col key={product.slug} sn={6} nd={4} lg={3}>
             <img src={product.image} alt={product.name} className='product-image' />
               <h2>{product.name}</h2>
               <p>₱{product.price}</p>
-          </li>
+          </Col>
         ))}
-        </ul>
+        </Row>
+        </Container>
+        
       </main>
-      <footer>All right reserved</footer>
+      <footer>
+        <div className='text-center'>All right reserved</div>
+        </footer>
     </div>
   );
 }
